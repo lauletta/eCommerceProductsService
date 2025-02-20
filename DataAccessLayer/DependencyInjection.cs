aee161b8f9d440bc44e2e339857a7d6514dac55b
lauletta <luigiauletta22@gmailcom>
2025-02-20 12:11:11 +0100

    Changed connection mode with env variables

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -11,10 +11,15 @@ public static class DependencyInjection
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<IProdcutRepository, ProductsRepository>();
+
+        string connectionString = configuration.GetConnectionString("MySqlConnection")!;
+
+        string connection = connectionString.Replace("$SQL_HOST", Environment.GetEnvironmentVariable("SQL_HOST"))
+            .Replace("$PASSWORD", Environment.GetEnvironmentVariable("PASSWORD"));
         services.AddDbContext<ProductDbContext>
             (options =>
             {
-                options.UseMySQL(configuration.GetConnectionString("MySqlConnection")!);
+                options.UseMySQL(connection);
             });
         return services;
     }
